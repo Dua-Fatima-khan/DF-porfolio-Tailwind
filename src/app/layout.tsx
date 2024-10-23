232f5d461f19d54e50b10dac87e744c7f6c00bb0
Dua-Fatima-khan <officialduafatima64648@gmail.com>
2024-10-23 23:42:44 +0500

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${playfair.className} ${montserrat.className} `}
       >
-        <Header />
+        <Header/>
         {children}
         <FOOTER/>
       </body>
