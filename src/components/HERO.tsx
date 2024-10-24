f4a1e2527deebcfcb3392dbf198a1d419567cf41
Dua-Fatima-khan <officialduafatima64648@gmail.com>
2024-10-24 22:48:06 +0500

    Update

--- src/components/HERO.tsx
+++ src/components/HERO.tsx
@@ -7,11 +7,11 @@ export default function Hero(){
         sm:text-5xl
         md:text-7xl
         lg:text-8xl 
-        ">Web Developer & </h1>
+        ">Web Developer</h1>
         <h2 className=" text-5xl
          sm:text-5xl
         md:text-7xl
-        lg:text-9xl">Designer</h2>
+        lg:text-9xl">  &  Designer</h2>
         <p className="w-[80vw] text-center py-10  leading-loose ">
             {` I'm a Pakistan-based web developer and designer, building websites that don’t just work—they slap! Whether it’s front-end magic or designs that make you go ‘damn!’, I’ve got the skills to bring your ideas to life. Let’s create something so epic, your browser will do a double take!`}
         </p>
