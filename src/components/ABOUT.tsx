e31eb5c66b3b1766f604356f56985745e7c40421
Dua-Fatima-khan <officialduafatima64648@gmail.com>
2024-10-24 22:22:22 +0500

    commited

--- src/components/ABOUT.tsx
+++ src/components/ABOUT.tsx
@@ -1,7 +1,7 @@
 export default function About () { 
     return (
      <section className="bg-black flex w-full justify-center items-center flex-col p-10 py-32">
-        <h2  className="text-[100px] italic ">Dua Fatima.</h2>
+        <h2  className="text-7xl italic ">Dua Fatima.</h2>
         <p className="w-[80vw] text-center leading-loose">
         I’m a <span className="playfair font-bold italic text-[17px]">Pakistan-based Frontend Web Developer</span> and designer who <span className="playfair font-bold text-[17px] italic">loves</span> turning ideas into vibrant online experiences! With a knack for <span className="playfair font-bold italic text-[17px]">HTML, CSS, JavaScript, TypeScript, Tailwind CSS, Bootstrap, and Next.js</span>, I create sleek, reponsive websites that are not just pretty but also packed with functionality. My expertise extends to animations using <span className="playfair font-bold italic text-[17px]">GSAP, Scroll Trigger, AOS,</span> and <span className="playfair font-bold italic text-[17px]">Framer Motion</span>, ensuring a dynamic user experience, along with proficiency in version control with <span className="playfair font-bold italic text-[17px]">Git</span> and <span className="playfair font-bold italic text-[17px]">GitHub</span>. If you’re ready to bring your vision to life, let’s make some <span className="playfair font-bold italic text-[17px]">digital fireworks</span> together!
       </p>
